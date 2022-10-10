fe5e42c22700cb9dd3ea7b731214bb11de6b16ac
jehadab <jehad1996@hotmail.com>
2022-10-10 16:18:09 +0300

    HTTP APIs for Books get , update , delete , put

--- my-books/Controllers/BooksController.cs
+++ my-books/Controllers/BooksController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using my_books.Data.Models;
 using my_books.Data.Services;
 using my_books.Data.VeiwModels;
 
@@ -21,5 +23,37 @@ namespace my_books.Controllers
             _booksService.addBook(book);
             return Ok();
         }
+
+        [HttpPut("update-book-by-id/{id}")]
+        public IActionResult UpdateBookById(int id , [FromBody]BookVM book)
+        {
+            var updatedBook = _booksService.UpdateBookById(id, book);
+            return Ok(updatedBook);
+        }
+
+        [HttpDelete("delete-book-by-id/{id}")]
+        public IActionResult DeleteById(int id)
+        {
+            _booksService.DeleteBookById(id);
+            return Ok();
+        }
+        
+
+        [HttpGet("get-all-books")]
+        public IActionResult GetAllBooks()
+        {
+            var allBooks = _booksService.GetAllBooks();
+            return Ok(allBooks);
+
+        }
+        [HttpGet("get-book-by-id/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _booksService.GetBookById(id);
+            return Ok(book);
+        }
+
+        
+
     }
 }
